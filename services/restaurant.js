58491e1fd4f235fd4be1c106eebf365032cf6fbd
ntruter42 <ntruter42@gmail.com>
2023-09-11 11:46:35 +0200

    Implemented and fixed test for bookTable function

--- services/restaurant.js
+++ services/restaurant.js
@@ -1,44 +1,49 @@
 const restaurant = (db) => {
 
-    async function getTables() {
+	async function getTables() {
 		// get all the available tables
 		const query = `SELECT * FROM public.table_booking`;
 		return await db.manyOrNone(query);
-    }
-
-    async function bookTable(tableName) {
-        // book a table by name
-    }
-
-    async function getBookedTables() {
-        // get all the booked tables
-    }
-
-    async function isTableBooked(tableName) {
-        // get booked table by name
-    }
-
-    async function cancelTableBooking(tableName) {
-        // cancel a table by name
 	}
-	
+
+	async function bookTable(booking) {
+		const query = `SELECT * FROM public.table_booking WHERE table_name = '${booking.tableName}'`;
+		const table = await db.one(query);
+
+		if (booking.seats > table.capacity) {
+			return "capacity greater than the table seats";
+		}
+    }
+
+	async function getBookedTables() {
+		// get all the booked tables
+	}
+
+	async function isTableBooked(tableName) {
+		// get booked table by name
+	}
+
+	async function cancelTableBooking(tableName) {
+		// cancel a table by name
+	}
+
 	async function editTableBooking(username) {
 		// edit number_of_people and contact_number columns by username
 	}
 
-    async function getBookedTablesForUser(username) {
-        // get user table booking
-    }
+	async function getBookedTablesForUser(username) {
+		// get user table booking
+	}
 
-    return {
-        getTables,
-        bookTable,
-        getBookedTables,
-        isTableBooked,
-        cancelTableBooking,
-        editTableBooking,
-        getBookedTablesForUser
-    }
+	return {
+		getTables,
+		bookTable,
+		getBookedTables,
+		isTableBooked,
+		cancelTableBooking,
+		editTableBooking,
+		getBookedTablesForUser
+	}
 }
 
 export default restaurant;
